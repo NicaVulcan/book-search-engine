6db934fd9fb9e6bde19eda46048c9c1f59767a20
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-04-03 18:22:56 -0400

    initial typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,19 @@
 const resolvers = {
     Query: {
-        me: () => {
-            return "it worked!"
+        // find user
+        user: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id})
+                .select('-__v -password')
+                .populate('books');
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+        // find books by user
+        books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params);
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,26 @@
 //import gql
 const { gql } = require('apollo-server-express');
 
+// type defs
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
     type Query {
-        me: String
+        user(username: String!): User
+        books(username: String!): [Book]
     }`;
 
 module.exports = typeDefs;
